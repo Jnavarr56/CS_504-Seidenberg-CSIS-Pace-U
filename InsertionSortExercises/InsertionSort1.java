6960ca5eed8d11bf9a6f03173c9c6fff9b6386e7
Jorge Navarro <jnavarr56@gmail.com>
2019-04-22 20:32:34 -0400

    practiced insertion sort.

---
+++ InsertionSortExercises/InsertionSort1.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class InsertionSort1 {
+
+    public InsertionSort1(int[] arr) {
+
+        int end = 0;
+
+        int toInsert = arr[0];
+
+        int shiftCount = 0;
+
+        int temp = 0;
+
+        boolean shouldShift;
+
+        while (end < arr.length) {
+
+            shouldShift = false;
+
+            for (int i = 0; i <= end; i++) {
+
+                if (arr[end] < arr[i]) {
+
+                    toInsert = arr[end];
+
+                    shouldShift = true;
+
+                    break;
+
+                }
+
+            }
+
+            if (shouldShift) {
+
+                shiftCount = end;
+
+                while (shiftCount > 0) {
+
+                    temp = arr[shiftCount - 1];
+
+                    arr[shiftCount] = temp;
+
+                    System.out.println(arr[shiftCount]);
+
+                    shiftCount--;
+
+                }
+
+                arr[0] = toInsert;
+
+            }
+
+            end++;
+
+            System.out.println(Arrays.toString(arr));
+
+        }
+
+    }
+
+}
---
+++ InsertionSortExercises/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String args[]) {
+
+        InsertionSort1 asd = new InsertionSort1(new int[]{7,5,8,2,1});
+
+
+    }
+
+}
